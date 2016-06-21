1c8f3c2c124134aad292a17ffcdc9bbaa08b82ad
crazymouse <crazymousethief@outlook.com>
2016-06-21 19:19:55 +0800

    Add ResizablePolygon

---
+++ src/pers/crazymouse/algorithm/visualizer/ResizablePolygon.java
@@ -0,0 +1,60 @@
+package pers.crazymouse.algorithm.visualizer;
+
+import javafx.geometry.Orientation;
+import javafx.scene.shape.Polygon;
+
+/**
+ * Created by crazymouse on 6/21/16.
+ */
+public class ResizablePolygon extends Polygon {
+    private double currentWidth;
+    private double currentHeight;
+
+    public ResizablePolygon() {
+    }
+
+    @Override
+    public void resize(double width, double height) {
+        computeSize();
+        System.out.println(width + " " + height);
+        if (width == 0 || height == 0) return;
+        double zoomWidthRate = width / currentWidth;
+        double zoomHeightRate = height / currentHeight;
+        System.out.println(zoomWidthRate + " " + zoomHeightRate);
+        for (int i = 0; i < getPoints().size(); i += 2) {
+            double w = getPoints().get(i);
+            double h = getPoints().get(i + 1);
+            getPoints().set(i, w * zoomWidthRate);
+            getPoints().set(i + 1, h * zoomHeightRate);
+        }
+    }
+
+    double boundedSize(double value, double min, double max) {
+        // if max < value, return max
+        // if min > value, return min
+        // if min > max, return min
+        return Math.min(Math.max(value, min), Math.max(min, max));
+    }
+
+    public void computeSize() {
+        Orientation contentBias = getContentBias();
+        double w, h;
+        if (contentBias == null) {
+            w = boundedSize(prefWidth(-1), minWidth(-1), maxWidth(-1));
+            h = boundedSize(prefHeight(-1), minHeight(-1), maxHeight(-1));
+        } else if (contentBias == Orientation.HORIZONTAL) {
+            w = boundedSize(prefWidth(-1), minWidth(-1), maxWidth(-1));
+            h = boundedSize(prefHeight(w), minHeight(w), maxHeight(w));
+        } else {
+            h = boundedSize(prefHeight(-1), minHeight(-1), maxHeight(-1));
+            w = boundedSize(prefWidth(h), minWidth(h), maxWidth(h));
+        }
+        currentWidth = w;
+        currentHeight = h;
+    }
+
+    @Override
+    public boolean isResizable() {
+        return true;
+    }
+}
